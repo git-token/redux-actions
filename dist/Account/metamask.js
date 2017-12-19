a2331437f3a0facffd1766aa62b6a8f46744442c
Ryan Tate <ryan.michael.tate@gmail.com>
2017-12-18 21:35:40 -0800

    WIP

--- dist/Account/metamask.js
+++ dist/Account/metamask.js
@@ -4,12 +4,26 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = metamask;
+
+var _bluebird = require('bluebird');
+
+var _bluebird2 = _interopRequireDefault(_bluebird);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 function metamask() {
   var _this = this;
 
   return function (dispatch) {
     if (typeof window.web3 !== 'undefined') {
       _this.web3 = new Web3(window.web3.currentProvider);
+      _this.eth = (0, _bluebird.promisifyAll)(_this.web3.eth);
+      _this.eth.getAccount().then(function (address) {
+        alert('Found Address: ' + address);
+      }).catch(function (error) {
+        console.log('error', error);
+        alert('MetaMask Account must be unlocked');
+      });
     } else {
       alert('Could not find MetaMask Plug-in, dispatch alert');
     }
--- src/Account/metamask.js
+++ src/Account/metamask.js
@@ -1,7 +1,16 @@
+import Promise, { promisifyAll } from 'bluebird'
+
 export default function metamask() {
   return (dispatch) => {
     if (typeof window.web3 !== 'undefined') {
       this.web3 = new Web3(window.web3.currentProvider)
+      this.eth = promisifyAll(this.web3.eth)
+      this.eth.getAccount().then((address) => {
+        alert(`Found Address: ${address}`)
+      }).catch((error) => {
+        console.log('error', error)
+        alert('MetaMask Account must be unlocked')
+      })
     } else {
       alert('Could not find MetaMask Plug-in, dispatch alert')
     }
