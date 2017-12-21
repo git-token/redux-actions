2b40c2e9b4d223a89cd3bb6760eadb8648295b59
Ryan Tate <ryan.michael.tate@gmail.com>
2017-12-20 19:58:02 -0800

    WIP

---
+++ dist/Account/setupStep.js
@@ -0,0 +1,19 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = setupStep;
+function setupStep(_ref) {
+  var step = _ref.step;
+
+  return function (dispatch) {
+    dispatch({
+      type: 'SET_ACCOUNT_SETUP_DETAILS',
+      id: 'activeStep',
+      value: step
+    });
+
+    location.href = '/setup' + step;
+  };
+}
